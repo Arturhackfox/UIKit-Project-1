b275ecc6f3ab5e959cdd7d92a6b96a2e74d631e7
Arthur Sh <arthur.shemshur@gmail.com>
2023-06-11 16:08:23 +0300

    Initial Commit

---
+++ proj-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  proj-1
+//
+//  Created by Arthur Sh on 11.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
